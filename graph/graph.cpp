650db846e5617afefb5187fad3a29b90f23a1bc0
Aaron McDaid <aaronmcdaid@erdos.ucd.ie>
2011-06-01 07:47:56 +0100

    (submodule), using the 'graph' submodule, instead of the old independent copy

--- graph/graph.cpp
+++
@@ -1,9 +0,0 @@
-#include "graph.hpp"
-namespace graph {
-
-neighbouring_node_id_iterator neighbouring_node_id_iterator :: end_marker() const {
-	const neighbouring_node_id_iterator ender;
-	return ender;
-}
-
-} // namespace graph
