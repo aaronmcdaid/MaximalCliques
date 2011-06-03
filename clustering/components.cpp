e236777ea955c7599da522e0e46378a1a31f6b33
Aaron McDaid <aaronmcdaid@gmail.com>
2011-06-03 22:47:24 +0100

    components :: move_node, not tested much

--- clustering/components.cpp
+++ clustering/components.cpp
@@ -20,4 +20,44 @@ namespace clustering {
 			this->my_iterator.at(i) = it;
 		}
 	}
+	int32_t components :: my_component_id(const int32_t n) const {
+		assert(n>=0 && n<N);
+		return this -> my_component.at(n);
+	}
+	const misc :: list_with_constant_size <int32_t> & components :: get_members(int32_t component_id) const {
+		assert(component_id >= 0 && component_id < this->N);
+		return this->members.at(component_id);
+	}
+	void components :: move_node(const int32_t node_id, const int32_t new_component_id) {
+		assert(new_component_id >= 0 && new_component_id < this->N);
+		const int32_t old_component_id = this->my_component_id(node_id);
+		assert(old_component_id != new_component_id);
+
+		this->my_component.at(node_id) = new_component_id;
+		const std :: list<int32_t> :: iterator it = this->my_iterator.at(node_id);
+		assert( *it == node_id );
+		{
+			const int32_t old_size = this->members.at(old_component_id).size();
+			this->members.at(old_component_id).erase(it);
+			const int32_t new_size = this->members.at(old_component_id).size();
+			assert(new_size == old_size-1);
+			if(new_size == 0)
+				this->empty_components.push(old_component_id);
+		}
+		{
+			const int32_t old_size = this->members.at(new_component_id).size();
+			if(old_size == 0) {
+				// this *was* empty. We must remove it from the empty_components stack,
+				// therefore we insist that it is on the top of that stack
+				assert(!this->empty_components.empty() && new_component_id == this->empty_components.top());
+				this->empty_components.pop();
+			}
+			this->members.at(new_component_id).push_front(node_id);
+			const std :: list<int32_t> :: iterator it = this->members.at(new_component_id).begin();
+			assert( *it == node_id );
+			this->my_iterator.at(node_id) = it;
+			const int32_t new_size = this->members.at(new_component_id).size();
+			assert(new_size == old_size+1);
+		}
+	}
 } // namespace clustering
--- clustering/components.hpp
+++ clustering/components.hpp
@@ -1,8 +1,10 @@
 #ifndef _CLUSTERING_COMPONENTS_
 #define _CLUSTERING_COMPONENTS_
 
+#include "../misc/list_with_constant_size.hpp"
+
 #include <vector>
-#include <list>
+#include <stack>
 #include <cstdlib>
 #include <cassert>
 
@@ -10,21 +12,17 @@ namespace clustering {
 
 class components {
 	int32_t N;
-	std :: vector< std :: list <int32_t> > members; // this will include a lot of empty lists
-	std :: vector< std :: list <int32_t> :: iterator > my_iterator; // for each node, the iterator to its position in all_members.
+	std :: vector< misc :: list_with_constant_size <int32_t> > members; // this will include a lot of empty lists
+	std :: vector< misc :: list_with_constant_size <int32_t> :: iterator > my_iterator; // for each node, the iterator to its position in all_members.
 	std :: vector< int32_t                           > my_component; // for each node, the id of the cluster it is in.
+	std :: stack< int32_t, std :: vector< int32_t > > empty_components;
 public:
 	components() : N(0) {}
 	void setN(const int32_t N);
 	void merge_components(const int32_t cl1, const int32_t cl2); // merge two non-empty components
-	int32_t my_component_id(const int32_t n) const {
-		assert(n>=0 && n<N);
-		return this -> my_component.at(n);
-	}
-	const std :: list <int32_t> & get_members(int32_t component_id) const {
-		assert(component_id >= 0 && component_id < this->N);
-		return this->members.at(component_id);
-	}
+	int32_t my_component_id(const int32_t n) const;
+	const misc :: list_with_constant_size <int32_t> & get_members(int32_t component_id) const;
+	void move_node(const int32_t node_id, const int32_t new_component_id);
 };
 	
 } // namespace clustering
