7dd94e79d25aee17dd92cbd6a6dce224ecf06d73
Aaron McDaid <aaronmcdaid@gmail.com>
2011-06-04 05:56:16 +0100

    clustering: typedef for members_list_type

--- clustering/components.hpp
+++ clustering/components.hpp
@@ -9,12 +9,13 @@
 #include <cassert>
 
 namespace clustering {
+	typedef misc :: list_with_constant_size<int32_t> member_list_type;
 
 class components {
 	int32_t N;
 	int32_t num_components; // always a bit bigger than N
-	std :: vector< misc :: list_with_constant_size <int32_t> > members; // this will include a lot of empty lists
-	std :: vector< misc :: list_with_constant_size <int32_t> :: iterator > my_iterator; // for each node, the iterator to its position in all_members.
+	std :: vector< member_list_type > members; // this will include a lot of empty lists
+	std :: vector< member_list_type :: iterator > my_iterator; // for each node, the iterator to its position in all_members.
 	std :: vector< int32_t                           > my_component; // for each node, the id of the cluster it is in.
 	std :: stack< int32_t, std :: vector< int32_t > > empty_components;
 public:
@@ -22,7 +23,7 @@ public:
 	void setN(const int32_t N);
 	void merge_components(const int32_t cl1, const int32_t cl2); // merge two non-empty components
 	int32_t my_component_id(const int32_t n) const;
-	const misc :: list_with_constant_size <int32_t> & get_members(int32_t component_id) const;
+	const member_list_type & get_members(int32_t component_id) const;
 	void move_node(const int32_t node_id, const int32_t new_component_id);
 	int32_t top_empty_component() const;
 };
