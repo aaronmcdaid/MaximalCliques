e2041604147ceee973ea1e12527022320f7260d8
Aaron McDaid <aaronmcdaid@erdos.ucd.ie>
2011-06-05 05:14:26 +0100

    Print less, and better, stats.

--- cp5.cpp
+++ cp5.cpp
@@ -388,7 +388,6 @@ static void do_clique_percolation_variant_5b(const int32_t min_k, const int32_t 
 	 */
 	for(int32_t k = min_k; k<=max_k /* we never actually reach this, see the `break` below*/; k++) {
 		vector<int32_t> found_communities; // the component_ids of the communities that will be found
-		PP2(k, ELAPSED);
 		const int32_t t = k-1;
 
 		intersecting_clique_finder isf(power_up);
@@ -397,10 +396,10 @@ static void do_clique_percolation_variant_5b(const int32_t min_k, const int32_t 
 				if(the_cliques.at(c).size() >= size_t(t))
 					isf.add_clique_to_bloom(the_cliques.at(c), c+power_up);
 			}
-			cout << "isf populated for k = " << k << ". " << HOWLONG << endl;
-			PPt(isf.get_bloom_filter().l);
-			PPt(isf.get_bloom_filter().calls_to_set);
-			PPt(isf.get_bloom_filter().occupied);
+			cout << "isf populated for k = " << k << ". "
+				<< " " << thou(isf.get_bloom_filter().occupied)
+				<< "/" << thou(isf.get_bloom_filter().l)
+				<< HOWLONG << endl;
 		}
 
 		one_k(
@@ -413,14 +412,13 @@ static void do_clique_percolation_variant_5b(const int32_t min_k, const int32_t 
 			, C
 			, isf
 			);
-		cout << HOWLONG << endl;
 		/* The found communities are now in found_communities. Gotta write them out
 		 */
 		write_all_communities_for_this_k(output_dir_name, k, found_communities, *current_percolation_level, the_cliques, network);
-		cout << HOWLONG << endl;
+
+		PP3(k, found_communities.size(), ELAPSED);
 
 		const int32_t new_k = k + 1;
-		PP(new_k);
 		if(new_k > max_k) {
 			delete current_percolation_level;
 			current_percolation_level = NULL;
@@ -434,7 +432,6 @@ static void do_clique_percolation_variant_5b(const int32_t min_k, const int32_t 
 		new_percolation_level->setN(C);
 
 		source_components.clear();
-		PP(__LINE__);
 		source_components_for_the_next_level (
 				source_components
 				, new_percolation_level
@@ -443,7 +440,6 @@ static void do_clique_percolation_variant_5b(const int32_t min_k, const int32_t 
 				, current_percolation_level
 				, the_cliques
 				);
-		PP(__LINE__);
 
 		swap(new_percolation_level, current_percolation_level);
 		delete new_percolation_level; // this is actually deleting the old_level, because of the swap on the immediately preceding line.
@@ -479,8 +475,8 @@ static void one_k (vector<int32_t> & found_communities
 
 while (!candidate_components.empty()) {
 	const int32_t source_component = candidate_components.back();
-	PP2(t+1, source_component);
-	cout << HOWLONG << endl;
+	// PP2(t+1, source_component);
+	// cout << HOWLONG << endl;
 	candidate_components.pop_back();
 	while(!current_percolation_level.get_members(source_component).empty()) { // keep pulling out communities from current source-component
 		// - find a clique that hasn't yet been assigned to a community
@@ -490,7 +486,7 @@ while (!candidate_components.empty()) {
 
 		assert(!current_percolation_level.get_members(source_component).empty());
 		const int32_t seed_clique = current_percolation_level.get_members(source_component).get().front();
-		PP(seed_clique);
+		// PP(seed_clique);
 		assert(assigned_branches.assigned_branches.at(power_up + seed_clique) == false);
 		assert(the_cliques.at(seed_clique).size() > size_t(t));
 
@@ -524,8 +520,8 @@ while (!candidate_components.empty()) {
 			// const int32_t new_size_of_growing_community = current_percolation_level.get_members(component_to_grow_into).size();
 			assert(frontier_cliques.size() < the_cliques.size());
 		}
-		const int32_t final_size_of_growing_community = current_percolation_level.get_members(component_to_grow_into).size();
-		PP2(t+1, final_size_of_growing_community);
+		// const int32_t final_size_of_growing_community = current_percolation_level.get_members(component_to_grow_into).size();
+		// PP2(t+1, final_size_of_growing_community);
 		// PP(calls_to_recursive_search);
 		found_communities.push_back(component_to_grow_into);
 	}
@@ -608,7 +604,7 @@ static void source_components_for_the_next_level (
 		) { // identify candidates for the next level
 	assert(source_components.empty());
 		for(int32_t f = 0; f < (int32_t) found_communities.size(); f++) {
-			PP2(f, ELAPSED);
+			// PP2(f, ELAPSED);
 			const int32_t new_cand = new_percolation_level->top_empty_component();
 			const clustering :: member_list_type & members_of_this_found_community = old_percolation_level->get_members(found_communities.at(f));
 			int32_t number_of_big_enough_cliques = 0;
